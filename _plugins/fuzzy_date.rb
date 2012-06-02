5ff10b891f060f6e78026c02af3cbe397a716209
Carlo Zottmann <carlo@municode.de>
2012-06-02 23:12:36 +0200

    [CHG] Monatsnamen-Format geändert.

--- _plugins/fuzzy_date.rb
+++ _plugins/fuzzy_date.rb
@@ -4,11 +4,11 @@ module Jekyll
   module FuzzyDateFilter
     def fuzzy_date(post_date)
       if post_date.day <= 10
-        str = "Early %h %Y"
+        str = "Early %B %Y"
       elsif post_date.day <= 20
-        str = "Mid %h %Y"
+        str = "Mid %B %Y"
       else
-        str = "Late %h %Y"
+        str = "Late %B %Y"
       end
 
       post_date.strftime(str)
