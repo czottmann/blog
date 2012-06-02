6eb5c4a6c74a9b96ca4d4bcbb4ab392a9b2f7ea2
Carlo Zottmann <carlo@municode.de>
2012-06-03 00:06:35 +0200

    [ADD] JS hinzugefügt. [ADD] Cache-Buster für CSS & JS eingebaut. [ADD] base-skeleton.min.css (base + skeleton, komprimiert) hinzugefügt.

---
+++ js/shenanigans.js
@@ -0,0 +1,17 @@
+var subline = document.getElementById("side-header-subline"),
+  claims = [
+    "Bavaria-based freelance web dev at day, beloved local vigilante \"Bavman\" at night.",
+    "From The People Who Brought You Breathable Atmosphere And Porn.",
+    "I Didn't Ask For This.",
+    "Entirely Unsuspicious Human Person.",
+    "Who writes these things?",
+    "Have you been to <a href='http://www.kiva.org/invitedby/czottmann' target='_blank'>Kiva</a> lately?  It's a Good Thing&trade;.",
+    "Today's lucky number is 22.",
+    "You look nice today.",
+    "You should really start to eat better.",
+    "A word of warning: not everything you'll read here is utterly brilliant.",
+    "Sometimes, I have memories of driving around Liberty City.  Makes me smile.",
+    "I was hooting when that Dragon capsule docked.  I love everything it stands for."
+  ];
+
+subline.innerHTML = claims[ Math.floor( Math.random() * claims.length ) ];
